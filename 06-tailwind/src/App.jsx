6d7613abb46147d997ff1efabce7be762537b59c
AdityaKumar <raoaditya9461@gmail.com>
2026-07-24 23:06:35 +0530

    Basic of tailwind

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900'>
+      Lorem ipsum dolor sit amet consectetur adipisicing Lorem ipsum dolor 
+      sit amet consectetur adipisicing elit. Incidunt sunt enim eligendi
+       perferendis tempore nulla culpa sequi qui et sapiente rem recusandae 
+       temporibus eum a blanditiis expedita, dolorem reiciendis ex.
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae c
+       onsequatur reprehenderit
+        fugiat cupiditate soluta autem ipsa laborum neque excepturi non
+         fugit rem repudiandae, perferendis nam at voluptatibus, quae ratione.
+    </div>
+  )
+}
+
+export default App
